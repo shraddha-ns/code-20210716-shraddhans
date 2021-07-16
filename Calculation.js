dbd1b0be2edc70102e3ae87a6520ceef939b0e46
shraddha-ns <87535357+shraddha-ns@users.noreply.github.com>
2021-07-16 23:20:13 +0530

    Add files via upload

---
+++ Calculation.js
@@ -0,0 +1,21 @@
+const calculateBMI = require('./BMI');
+
+const input = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, 
+                {"Gender": "Male", "HeightCm": 161, "WeightKg":85 }, 
+                {"Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, 
+                {"Gender": "Female", "HeightCm": 166,"WeightKg": 62}, 
+                {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, 
+                {"Gender": "Female","HeightCm": 167, "WeightKg": 82}]
+
+for(let i = 0; i < input.length; i++) 
+{
+  calculateBMI(input[i],input[i].HeightCm/100, input[i].WeightKg)
+}
+console.log('BMI output', input)
+
+var count = input.reduce(function (n, input) 
+{
+  return n + (input.bmiCategory == 'Overweight');
+}, 0);
+
+console.log('Total no. of Overweight people: ',count);
